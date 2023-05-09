db96eee07f603068d988b62b7333675a500d6557
sandalwoods <1764076874@qq.com>
2023-05-09 14:54:49 +0800

    Initial Commit

---
+++ FirbaseBootcamp/FirbaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirbaseBootcampApp.swift
+//  FirbaseBootcamp
+//
+//  Created by kevin on 2023/5/9.
+//
+
+import SwiftUI
+
+@main
+struct FirbaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
